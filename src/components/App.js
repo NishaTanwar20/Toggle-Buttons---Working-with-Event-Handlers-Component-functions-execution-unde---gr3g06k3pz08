145d4a405f7d3b3553875065bda2323994bf8c56
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-18 12:52:48 +0530

    assign value to buttons

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,22 @@ import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here 
-  
-
+  const [text , setText] = useState(false);
+const handleClick = () => {
+  setText(!text);
+}
   
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
       {/* assign value for button 1 */}
+      {text ? "OFF" : "ON"}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
         {/* assign value for button 2 */}
+        {text ? "ON" : "OFF"}
       </button>
     </div>
   );
